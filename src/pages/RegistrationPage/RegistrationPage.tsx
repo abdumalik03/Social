283f620fee75a323a99b37b6bde1fc599c9088a3
abdumalik03 <abdumalikabdurasulov74@gmail.com>
2024-04-12 21:26:35 +0500

    main

---
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom"
+import { LoginWith } from "../../components/LoginWith/LoginWith"
+import { AppHeading } from "../../components/Typography/AppHeading"
+import { AppButton } from "../../components/UI/AppButton/AppButton"
+import { AppInput } from "../../components/UI/AppInput/AppInput"
+import { SCAppInput } from "../../components/UI/AppInput/AppInput.styled"
+import { SCLoginPage } from "../LoginPage/LoginPage.styled"
+
+export const RegistrationPage = () => {
+    return(
+      <SCLoginPage>
+        <div className="LoginPage">
+      <AppHeading headingText="Регистрация" headingType={"h1"}/>
+      
+      <form action="#">
+        <AppInput inputType={"text"} inputPlaceholder={"Введите имя"}/>
+        <AppInput inputType={"email"} inputPlaceholder={"Почта"}/>
+        <AppInput inputType={"tel"} inputPlaceholder={"Номер телефона"}/>
+        <AppInput inputType={"password"} inputPlaceholder={"Пароль"}/>
+        <AppInput inputType={"text"} inputPlaceholder={"Город"}/>
+        
+        <AppButton buttonText={"Зарегистрироваться"}/>
+        
+      </form>
+      
+      <div className="registration">
+        <span>
+          Уже есть аккаунт? <Link to="/">Войти</Link>
+        </span>
+        <p>Регистрация с помощью</p>
+        <LoginWith/>
+        
+      </div>
+    </div>
+    </SCLoginPage>
+    )
+}
